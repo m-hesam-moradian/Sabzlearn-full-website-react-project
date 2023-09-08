935cf25a24b43bcdef6697d6fdb1768eabf07995
hesam <m.hesam.moradian@gmail.com>
2023-09-08 19:05:08 +0330

    courses

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
+import React from "react";
+import Header from "../../Components/Header/Header";
+import LastCourses from "../../Components/LastCourses/LastCourses";
 
-import './Index.css'
+import "./Index.css";
 
 export default function Index() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <LastCourses />
+    </>
+  );
 }
